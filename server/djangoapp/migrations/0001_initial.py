83de2bec7cd7011812c9ad85abce19bc14b363dd
donoli1st <46999623+donoli1st@users.noreply.github.com>
2025-12-12 03:34:06 +0100

    Refactor CarMake and CarModel migrations

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -17,17 +17,16 @@ class Migration(migrations.Migration):
             name='CarMake',
             fields=[
                 ('id', models.BigAutoField(
-                    auto_created=True, 
-                    primary_key=True, 
-                    serialize=False, 
-                    verbose_name='ID'
-                )
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID')
                 ),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField(blank=True)),
                 ('country', models.CharField(
-                    blank=True, 
-                    max_length=100, 
+                    blank=True,
+                    max_length=100,
                     null=True)
                 ),
             ],
@@ -36,37 +35,37 @@ class Migration(migrations.Migration):
             name='CarModel',
             fields=[
                 ('id', models.BigAutoField(
-                    auto_created=True, 
-                    primary_key=True, 
-                    serialize=False, 
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
                     verbose_name='ID')
                 ),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
                 ('type', models.CharField(
                     choices=[
-                        ('SEDAN', 'Sedan'), 
-                        ('SUV', 'SUV'), 
-                        ('WAGON', 'Wagon'), 
-                        ('COUPE', 'Coupe'), 
+                        ('SEDAN', 'Sedan'),
+                        ('SUV', 'SUV'),
+                        ('WAGON', 'Wagon'),
+                        ('COUPE', 'Coupe'),
                         ('TRUCK', 'Truck')
-                    ], 
-                    default='SUV', 
+                    ],
+                    default='SUV',
                     max_length=10)
                 ),
                 ('year', models.IntegerField(
-                    validators=[django.core.validators.MinValueValidator(2015), 
+                    validators=[django.core.validators.MinValueValidator(2015),
                                 django.core.validators.MaxValueValidator(2023)]
                 )
                 ),
                 ('color', models.CharField(
-                    blank=True, 
-                    max_length=50, 
+                    blank=True,
+                    max_length=50,
                     null=True)
                 ),
                 ('car_make', models.ForeignKey(
-                    on_delete=django.db.models.deletion.CASCADE, 
-                    related_name='models', 
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='models',
                     to='djangoapp.carmake')
                 ),
             ],
